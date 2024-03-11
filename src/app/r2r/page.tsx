4fa87d75b09b23323c125028f3af58e52a7cdfce
nathan kim <nathanckim18@gmail.com>
2024-03-11 14:18:36 -0400

    updated elo

--- src/app/r2r/page.tsx
+++ src/app/r2r/page.tsx
@@ -2,7 +2,7 @@ import R2R from "../campaigns/r2r/page";
 import Layout from "../campaigns/layout";
 
 // horrible, disgusting
-export default () => {
+export default function R2R() => {
   return (
     <Layout>
       <R2R />
