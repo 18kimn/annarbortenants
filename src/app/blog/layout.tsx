73aa9813566f7639da49be6b6817bf61ee101614
nathan kim <nathanckim18@gmail.com>
2024-05-23 14:37:03 -0400

    annyoing type error

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,10 +1,10 @@
 "use client";
 import styles from "./layout.module.css";
 
-export default function Layout(props) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className={styles.container}>
-      <div className={styles.content}>{props.children}</div>;
+      <div className={styles.content}>{children}</div>;
     </div>
   );
 }
