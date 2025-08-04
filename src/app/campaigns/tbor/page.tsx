400651b7afae8e46c18c46c81190cd9feeee45d9
nathan kim <nathanckim18@gmail.com>
2025-08-04 02:31:47 -0400

    netlify config

--- src/app/campaigns/tbor/page.tsx
+++ src/app/campaigns/tbor/page.tsx
@@ -1,9 +1,10 @@
-export const dynamic = 'force-static'
 import fs from 'fs'
 import path from 'path'
 import * as cheerio from 'cheerio'
 import classes from './page.module.css'
 import Image from 'next/image'
+
+export const dynamic = 'force-static'
 
 const topIcon = `
 <svg style="height: 0.8em;" viewBox="0 0 24 24"><path d="M6 17.59 7.41 19 12 14.42 16.59 19 18 17.59l-6-6z"></path><path d="m6 11 1.41 1.41L12 7.83l4.59 4.58L18 11l-6-6z"></path></svg>
